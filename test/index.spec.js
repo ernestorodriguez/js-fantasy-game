f2ed26fa972044a87819c03f5f567c7b2c804558
Ernest R <circuloconcentrico@gmail.com>
2020-10-19 09:12:52 -0300

    added full team lineup check and fixes

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,21 +2,81 @@
 import Player from "../lib/player";
 import QB from "../lib/positions/QB";
 import RB from "../lib/positions/RB";
+import FLEX from "../lib/positions/FLEX";
+import WR from "../lib/positions/WR";
+import TE from "../lib/positions/TE";
+import DST from "../lib/positions/DST";
 
 describe('Fantasy Game', () => {
     it('the user should be enable create a FL lineUp and add players', () => {
         const lineUp = new Lineup([
             new QB(),
+            new RB(),
+            new WR(),
+            new TE(),
+            new FLEX(),
+            new DST(),
         ]);
-        const player = new Player({
+        const playerA = new Player({
             name: 'A',
             position: QB
         });
-        lineUp.add(player)
-        expect(lineUp.toObject()).toEqual({ positions:[{
-            name: 'QB',
-            player: 'A',
-        }]});
+        const playerB = new Player({
+            name: 'B',
+            position: RB
+        });
+        const playerC = new Player({
+            name: 'C',
+            position: WR
+        });
+        const playerD = new Player({
+            name: 'D',
+            position: TE
+        });
+        const playerE = new Player({
+            name: 'E',
+            position: RB
+        });
+        const playerF = new Player({
+            name: 'F',
+            position: DST
+        });
+        
+        lineUp.add(playerA);
+        lineUp.add(playerB)
+        lineUp.add(playerC)
+        lineUp.add(playerD)
+        lineUp.add(playerE)
+        lineUp.add(playerF)
+
+        expect(lineUp.toObject()).toEqual({ 
+            positions:[
+                {
+                    name: 'QB',
+                    player: 'A',
+                },
+                {
+                    name: 'RB',
+                    player: 'B',
+                },
+                {
+                    name: 'WR',
+                    player: 'C',
+                },
+                {
+                    name: 'TE',
+                    player: 'D',
+                },
+                {
+                    name: 'FLEX',
+                    player: 'E',
+                },
+                {
+                    name: 'DST',
+                    player: 'F',
+                },
+            ]
+        });
     });
     
     it('shouldn\'t add players to assigned positions', () => {
@@ -31,8 +91,15 @@ describe('Fantasy Game', () => {
             name: 'B',
             position: QB
         });
+        const playerC = new Player({
+            name: 'C',
+            position: RB
+        });
+        
         lineUp.add(playerA);
         lineUp.add(playerB);
+        lineUp.add(playerC);
+        
         expect(lineUp.toObject()).toEqual({ positions:[{
             name: 'QB',
             player: 'A'
@@ -60,5 +127,83 @@ describe('Fantasy Game', () => {
                 name: 'RB'
             },
         ]});
+    });
+
+    describe('should possible to assign only RB, WR and TE players on FLEX position', () => {
+       it('QB and DST', () => {
+            const lineUp = new Lineup([
+                new FLEX(),
+            ]);
+            const playerA = new Player({
+                name: 'A',
+                position: QB
+            });
+           const playerB = new Player({
+               name: 'A',
+               position: DST
+           });
+            
+            lineUp.add(playerA);
+            expect(lineUp.toObject()).toEqual(
+                { positions:[
+                    {
+                        name: 'FLEX',
+                    },
+            ]});
+        });
+        it('RB', () => {
+            const lineUp = new Lineup([
+                new FLEX(),
+            ]);
+            const playerA = new Player({
+                name: 'A',
+                position: RB
+            });
+            
+            lineUp.add(playerA);
+            expect(lineUp.toObject()).toEqual(
+            { positions:[
+                    {
+                        name: 'FLEX',
+                        player: 'A',
+                    },
+            ]});
+        });
+        it('WR', () => {
+            const lineUp = new Lineup([
+                new FLEX(),
+            ]);
+            const playerA = new Player({
+                name: 'A',
+                position: WR
+            });
+
+            lineUp.add(playerA);
+            expect(lineUp.toObject()).toEqual(
+                { positions:[
+                        {
+                            name: 'FLEX',
+                            player: 'A',
+                        },
+                    ]});
+        });
+        it('TE', () => {
+            const lineUp = new Lineup([
+                new FLEX(),
+            ]);
+            const playerA = new Player({
+                name: 'A',
+                position: TE
+            });
+
+            lineUp.add(playerA);
+            expect(lineUp.toObject()).toEqual(
+                { positions:[
+                        {
+                            name: 'FLEX',
+                            player: 'A',
+                        },
+                    ]});
+        });
     });
 })
